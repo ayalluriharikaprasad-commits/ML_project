a51f7e2015f662c31103874b14110f50b2ee8554
kallurivenkatesh4416-commits <225694925+kallurivenkatesh4416-commits@users.noreply.github.com>
2026-05-04 15:32:45 +0530

    Add project files

--- template.py
+++ template.py
@@ -1,3 +1,129 @@
-import numpy as np
 import os
-import matplotlib.pyplot as plt
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')  
+
+project_name = "mlProject"
+
+
+
+list_of_files = [
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f'src/{project_name}/config/__init__.py',
+    f'src/{project_name}/config/configuration.py',
+    f'src/{project_name}/pipeline/__init__.py',
+    f'src/{project_name}/entity/__init__.py',
+    f'src/{project_name}/entity/config_entity.py',
+    f'src/{project_name}/constants/__init__.py',
+    'config/config.yaml',
+    'params.yaml',
+    'main.py',
+    'schema.yaml',
+    'app.py',
+    'requirements.txt',
+    'setup.py',
+    'research/trials.ipynb',
+    'templates/index.html'
+]
+
+
+# Now lets create the files
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok = True)
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+        logging.info(f"Creating file: {filepath}")
+    else:
+        logging.info(f"File already exists: {filepath}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
