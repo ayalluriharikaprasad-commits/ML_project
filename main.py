1419c85f21f6a5bfd689c52b637b3b63c2bcfe5d
kallurivenkatesh4416-commits <225694925+kallurivenkatesh4416-commits@users.noreply.github.com>
2026-05-06 11:08:42 +0530

    added the data ingestion pipeline

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from src.mlProject.logging import logger
+from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info('This is our custom log')
+STAGE_NAME = 'Data Ingestion stage'
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise
