41eb5fd59314be79fccad3ce584a434c46854035
kallurivenkatesh4416-commits <225694925+kallurivenkatesh4416-commits@users.noreply.github.com>
2026-05-07 13:15:23 +0530

    added model training pipeline

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationPipeline
-
+from mlProject.pipeline.stage_04_model_trainer import ModelTrainerPipeline
 
 STAGE_NAME = 'Data Ingestion stage'
 
@@ -37,6 +37,17 @@ STAGE_NAME = 'Data transformation stage'
 try:
     logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
     obj = DataTransformationPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Model training stage'
+
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
+    obj = ModelTrainerPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 except Exception as e:
