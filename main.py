7c30ddc0171fefbb02898010b9de4da557168969
kallurivenkatesh4416-commits <225694925+kallurivenkatesh4416-commits@users.noreply.github.com>
2026-05-07 10:55:02 +0530

    added data transformation pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationPipeline
 
 
 STAGE_NAME = 'Data Ingestion stage'
@@ -16,6 +17,7 @@ except Exception as e:
 
 
 
+
 STAGE_NAME = 'Data validation stage'
 
 try:
@@ -26,3 +28,18 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+
+STAGE_NAME = 'Data transformation stage'
+
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
+    obj = DataTransformationPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
